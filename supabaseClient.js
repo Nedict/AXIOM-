4264d111aeb06235a743b4b122fc1b340b9a2da1
Bright Ajiboye <155493086+Nedict@users.noreply.github.com>
2026-06-25 07:28:37 +0100

    Fix Supabase client initialization variables

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://mqygivjwarphxxjxltn.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_qNMD-uuGWGlbvdH7kUYbsw_mSB9pgsQ';
+// We use the raw strings directly in Vanilla JS
+const supabaseUrl = 'https://mgqygivjwarphxxjxltn.supabase.co';
+const supabaseKey = 'sb_publishable_qNMD-uuGWGlbvdH7kUYbsw_mSB9pgsQ';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
